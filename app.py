9d819033e76e016b7f390b18ada014529ebab21f
Ezra Adeyinka <65364356+adeyinkaezra123@users.noreply.github.com>
2023-10-31 18:08:46 +0000

    fix: fix authentication logic

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from streamlit_tags import st_tags
 import streamlit as st
 import mimetypes
-import pandas as pd
 from datetime import datetime, timedelta
-import plotly.graph_objects as go
 from utils import convert_to_days
 from middlewares.mindsdb import MindsDB
 
 
 MAX_PACKAGES = 5
 PREDICTION_OPTIONS = ('1 Week', '1 Month', "3 Months", '6 Months', '1 Year', "2 Years")
-MINDSDB_USERNAME = st.secrets['MINDSDB_USERNAME']
-MINDSDB_PASSWORD = st.secrets['MINDSDB_PASSWORD']
+MINDSDB_USERNAME = st.secrets['mindsdb_username']
+MINDSDB_PASSWORD = st.secrets['mindsdb_password']
 
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
@@ -20,9 +18,8 @@ mimetypes.add_type('text/css', '.css')
 st.set_page_config(page_title="NPM Forecast", page_icon="✨", layout="wide")
 
 
-# mindsdb_instance = MindsDB(email=MINDSDB_USERNAME, password=MINDSDB_PASSWORD)
-mindsdb_instance = MindsDB()
-
+mindsdb_instance = MindsDB(MINDSDB_USERNAME, MINDSDB_PASSWORD)
+# mindsdb_instance = MindsDB()
 
 
 with open('style.css') as f:
@@ -38,62 +35,14 @@ with st.sidebar:
         key='1')
     st.markdown("<h3>In the next</h3>",unsafe_allow_html=True)
     time_options = st.selectbox(
-    '',
+    'Select time frame',
     PREDICTION_OPTIONS
     )
-    result = st.button(label="Predict", help=None, on_click= mindsdb_instance.collect_database, args=None, kwargs=None, type="primary", disabled=False, use_container_width=False)
-    st.write(result)
-    # st.button(label="Predict", help=None, on_click= mindsdb_instance.get_package_data, args=('requests', "1 Week"), kwargs=None, type="primary", disabled=False, use_container_width=False)
+    st.button(label="Predict", help=None, on_click= mindsdb_instance.get_package_data, args=('requests', "1 Week"), kwargs=None, type="primary", disabled=False, use_container_width=False)
 
-# with st.container():
-#     print('')
-#     fig = get_package_data('requests', '1 Week')
-#     st.plotly_chart(fig, use_container_width=True)
+with st.container():
+    print('')
+    fig = mindsdb_instance.get_package_data('requests', '1 Week')
+    st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
 
 
-
-
-
-
-
-
-
-# def get_package_data(package: Union[str, List[str]], prediction_days: str):
-#     database = databases[-1]
-#     query = database.query(
-#     f'SELECT date, downloads FROM pypi_datasource.overall WHERE package="{package}" AND mirrors=true limit 500'
-# )
-#     overall_df = query.fetch()
-
-#     converted_prediction_days = convert_to_days(prediction_days)
-#     predicted_dataframe = pd.DataFrame(columns=["date", "downloads"])
-#     today = datetime.today()
-#     prediction_date = (today - timedelta(days=180)).date()
-#     for i in range(converted_prediction_days):
-#         query = database.query(
-#             f'SELECT date, downloads FROM mindsdb.pypi_model WHERE date="{prediction_date}"'
-#         )
-#         predicted_value = query.fetch()
-#         current_date = (today + timedelta(days=i)).date()
-#         predicted_dataframe = pd.concat([predicted_dataframe, query.fetch()], ignore_index=True)
-#     fig = go.Figure()
-#     fig.add_trace(
-#         go.Scatter(
-#             x=overall_df["date"], y=overall_df["downloads"], mode="lines", name="Data"
-#         )
-#     )
-#     fig.add_trace(
-#     go.Scatter(
-#         x=predicted_dataframe["date"],
-#         y=predicted_dataframe["downloads"],
-#         mode="lines",
-#         name="Prediction",
-#     )
-# )
-    # fig.update_layout(
-    #     title="PyPI Package Download Rate Prediction",
-    #     xaxis_title="Dates",
-    #     yaxis_title="Downloads",
-    #     template="plotly_dark",
-    # )
-    # return fig
--- middlewares/mindsdb.py
+++ middlewares/mindsdb.py
@@ -1,6 +1,9 @@
-from typing import Any
+from datetime import datetime, timedelta
+from typing import Any, Union, List
 import mindsdb_sdk
 import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
 from mindsdb_sdk.server import Server
 from mindsdb_sdk.databases import Database
 from pandas import DataFrame
@@ -8,7 +11,7 @@ from requests.exceptions import HTTPError, ConnectionError
 from exceptions.auth import CredentialsError
 from exceptions.connection import NetworkError
 from templates.mindsdb_queries import SQL_PACKAGE_QUERY
-
+from utils import *
 
 
 MINDSDB_HOST = 'https://cloud.mindsdb.com'
@@ -20,37 +23,37 @@ class MindsDB:
     MindsDB manager class
     """
 
-    def __init__(self) -> None:
-    # def __init__(self, email: str, password: str) -> None:
+    def __init__(self, email: str, password: str) -> None:
         """
         initializer class.
         Args:
             email: MindsDB account email address (that is stored as an env var)
             password: MindsDB account password
         """
-        # self.email = email
-        # self.password = password
+        self.email = email
+        self.password = password
 
         self.is_authenticated: bool = False
-        self.database: Database
+        self.database: Database = None
         self.server = None
         if not self.is_authenticated:
             self.authenticate()
-
+            
+        print(self.is_authenticated)
     def authenticate(self) -> None:
         """
         authorizes the email and password with MindsDB's host
         """
         try:
-            self.server = mindsdb_sdk.connect('http://127.0.0.1:47334')
+            # self.server = mindsdb_sdk.connect('http://127.0.0.1:47334')
 
-            # self.server = mindsdb_sdk.connect(
-            #     MINDSDB_HOST,
-            #     login=self.email,
-            #     password=self.password,
-            # )
-        except HTTPError:
-            raise CredentialsError('Email or password is incorrect. Make sure to enter the right credentials.')
+            self.server = mindsdb_sdk.connect(
+                MINDSDB_HOST,
+                login=self.email,
+                password=self.password,
+            )
+        except HTTPError as e:
+            raise CredentialsError('Email or password is incorrect. Make sure to enter the right credentials.', e)
         except ConnectionError as e:
             raise NetworkError('Make sure you have access to the internet and try again.', e)
 
@@ -59,69 +62,53 @@ class MindsDB:
 
     def collect_database(self) -> Database:
         if self.server:
-            database = self.server.get_database('pypi_datasource')
-            print(database)
-            return database
+            self.database = self.server.get_database('pypi_datasource')
+            return self.database
 
-    # def answer(self, question: str) -> Any:
-    #     """
-    #     takes the question and queries then converts the response into `rich.Markdown`
-    #     Args:
-    #         question: the value from `ask` positional argument
+    def get_package_data(self, package: Union[str, List[str]], prediction_days: str):
+        if type(package) == 'list':
+            pass
+        query = self.database.query(
+        f'SELECT date, downloads FROM pypi_datasource.overall WHERE package="{package}" AND mirrors=true limit 500'
+    )
+        overall_df = query.fetch()
 
-    #     Returns:
-    #         response from MindsDB in Markdown format
-    #     """
-
-    #     return to_data(
-    #         self.database.query(
-    #             SQL_ASK_QUERY.substitute(
-    #                 ask=question,
-    #                 user=getuser(),
-    #             )
-    #         ).fetch()
-    #     )
-    
-
-
-# def get_package_data(package: Union[str, List[str]], prediction_days: str):
-#     server = mindsdb_sdk.connect('https://cloud.mindsdb.com', login=MINDSDB_USERNAME, password=MINDSDB_PASSWORD)
-#     databases = server.list_databases()
-#     database = databases[-1]
-#     query = database.query(
-#     f'SELECT date, downloads FROM pypi_datasource.overall WHERE package="{package}" AND mirrors=true limit 500'
-# )
-#     overall_df = query.fetch()
-
-#     converted_prediction_days = convert_to_days(prediction_days)
-#     predicted_dataframe = pd.DataFrame(columns=["date", "downloads"])
-#     today = datetime.today()
-#     prediction_date = (today - timedelta(days=180)).date()
-#     for i in range(converted_prediction_days):
-#         query = database.query(
-#             f'SELECT date, downloads FROM mindsdb.pypi_model WHERE date="{prediction_date}"'
-#         )
-#         predicted_value = query.fetch()
-#         current_date = (today + timedelta(days=i)).date()
-#         predicted_dataframe = pd.concat([predicted_dataframe, query.fetch()], ignore_index=True)
-#     fig = go.Figure()
-#     fig.add_trace(
-#         go.Scatter(
-#             x=overall_df["date"], y=overall_df["downloads"], mode="lines", name="Data"
-#         )
-#     )
-#     fig.add_trace(
-#     go.Scatter(
-#         x=predicted_dataframe["date"],
-#         y=predicted_dataframe["downloads"],
-#         mode="lines",
-#         name="Prediction",
-#     )
-# )
-#     fig.update_layout(
-#         title="PyPI Package Download Rate Prediction",
-#         xaxis_title="Dates",
-#         yaxis_title="Downloads",
-#         template="plotly_dark",
-#     )
-#     return fig
+        converted_prediction_days = convert_to_days(prediction_days)
+        predicted_dataframe = pd.DataFrame(columns=["date", "downloads"])
+        today = datetime.today()
+        prediction_date = (today - timedelta(days=180)).date()
+        for i in range(converted_prediction_days):
+            query = self.database.query(
+                f'SELECT date, downloads FROM mindsdb.pypi_model WHERE date="{prediction_date}"'
+            )
+            predicted_value = query.fetch()
+            current_date = (today + timedelta(days=i)).date()
+            predicted_dataframe = pd.concat([predicted_dataframe, query.fetch()], ignore_index=True)
+        fig = go.Figure()
+        # fig.add_trace(
+        #     go.Scatter(
+        #         x=overall_df["date"], y=overall_df["downloads"], mode="lines", name="Data"
+        #     )
+        # )
+        fig.add_trace(
+        go.Scatter(
+            x=predicted_dataframe["date"],
+            y=predicted_dataframe["downloads"],
+            mode="lines",
+            name=f"{package.title()}",
+        )
+    )
+        fig.update_layout(
+            title="NPM Module Download Rate Prediction",
+            xaxis_title="Timeframe",
+            yaxis_title="Estimated Downloads",
+            template="plotly_dark",
+        )
+        print(fig)
+        return fig
+    # def get_multiple_package_data(self, packages: List[str], prediction_days: str):
+    #     fig = go.Figure()
+    #     for package in packages:
+    #         print(package)
+    #         single_fig = self.get_multiple_package_data(package, prediction_days)
+    #         fig.add
